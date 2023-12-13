e68dab309f2c44a08a5f95389022b79be925d9f2
stefanomarch <stefano.marchetti20@gmail.com>
2023-12-13 18:37:34 +0100

    initial setup

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../lib/hacker_news_scraper'
+
+RSpec.describe HackerNewsScraper do
+  describe '#initialize' do
+    it 'is a HNScraper' do
+      expect(HackerNewsScraper.new).to be_a(HackerNewsScraper)
+    end
+  end
+end
