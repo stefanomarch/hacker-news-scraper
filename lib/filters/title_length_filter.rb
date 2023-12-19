5e936441324d83b493dba71d96593ab52ed63c32
stefanomarch <stefano.marchetti20@gmail.com>
2023-12-19 12:02:54 +0100

    add sorting by fields to filterd array

--- lib/filters/title_length_filter.rb
+++ lib/filters/title_length_filter.rb
@@ -3,15 +3,19 @@
 module Filters
   # Usage example:
   # Filters::TitleLengthFilter.call(entries, :>, 5) # selects entries with titles longer than 5 words
-  # Filters::TitleLengthFilter.call(entries, :<=, 5) # selects entries with titles 5 words or shorter
   #
   # This filter allows to select entries with {dynamic operator (:>, :<, :<=)} {dynamic length}
   class TitleLengthFilter
-    class << self
-      def call(entries, operator, length)
-        entries.select do |entry|
-          entry[:title].split.length.send(operator, length)
-        end
+    attr_reader :entries
+
+    # Initializes with the filtered entries
+    def initialize(entries)
+      @entries = entries
+    end
+
+    def self.call(entries, operator, length)
+      entries.select do |entry|
+        entry[:title].split.length.send(operator, length)
       end
     end
   end
--- spec/lib/filters/title_length_filter_spec.rb
+++ spec/lib/filters/title_length_filter_spec.rb
@@ -2,6 +2,7 @@
 
 require 'spec_helper'
 require_relative '../../../lib/filters/title_length_filter'
+require 'byebug'
 
 ENTRIES_JSON = File.expand_path('../../fixtures/entries_array.rb', __dir__)
 
@@ -9,16 +10,16 @@ RSpec.describe Filters::TitleLengthFilter do
   describe '.call' do
     let(:entries) do
       [
-        { title: 'A short title' },
-        { title: 'A very long and descriptive title of an article' },
-        { title: 'Medium length title' },
-        { title: 'Tiny' },
-        { title: 'An adequately sized title' }
+        { title: 'A short title', points: 10, n_of_comments: 10 },
+        { title: 'A very long and descriptive title of an article', points: 11, n_of_comments: 0 },
+        { title: 'Medium length title', points: 7, n_of_comments: 20 },
+        { title: 'Tiny', points: 1, n_of_comments: 5 },
+        { rank: 5, title: 'An adequately sized title', points: 5, n_of_comments: 70 }
       ]
     end
 
     context 'when filtering with operator :>' do
-      let(:result) { described_class.call(entries, :>, 5) }
+      let(:result) { described_class.call(entries, :>, 5).entries }
 
       it 'returns entries with titles longer than the specified length' do
         expect(result.length).to eq(1)
@@ -27,14 +28,14 @@ RSpec.describe Filters::TitleLengthFilter do
 
     context 'when filtering with operator :<=' do
       it 'returns entries with titles equal to or shorter than the specified length' do
-        result = described_class.call(entries, :<=, 3)
+        result = described_class.call(entries, :<=, 3).entries
         expect(result.length).to eq(3)
       end
     end
 
     context 'when filtering with operator :=' do
       it 'returns entries with titles equal to or shorter than the specified length' do
-        result = described_class.call(entries, :==, 3)
+        result = described_class.call(entries, :==, 3).entries
         expect(result.length).to eq(2)
       end
     end
